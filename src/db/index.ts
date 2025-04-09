666ef15cd13bd13a82f29525f9d07e9a64d06262
hoywu <wu_git@outlook.com>
2025-04-09 10:26:23 +0800

    perf(db): enable WAL mode for SQLite database
    - Set journal mode to WAL in SQLite database
    - Ignore WAL-related files in .gitignore

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,4 +3,6 @@ import { drizzle } from 'drizzle-orm/bun-sqlite';
 import * as schema from './schema';
 
 const sqlite = new Database(Bun.env.DB_FILE_NAME);
+sqlite.exec('PRAGMA journal_mode = WAL;');
+
 export const db = drizzle({ client: sqlite, schema });
