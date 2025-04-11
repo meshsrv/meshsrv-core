f91eb4a5fed66a6da8bc99ac94fb37839a685cb4
hoywu <wu_git@outlook.com>
2025-04-11 18:39:49 +0800

    chore(env): ensure default database file name is used if not specified

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,7 @@ import { Database } from 'bun:sqlite';
 import { drizzle } from 'drizzle-orm/bun-sqlite';
 import * as schema from './schema';
 
-const sqlite = new Database(Bun.env.DB_FILE_NAME);
+const sqlite = new Database(Bun.env.DB_FILE_NAME || 'db.sqlite');
 sqlite.exec('PRAGMA journal_mode = WAL;');
 
 export const db = drizzle({ client: sqlite, schema });
