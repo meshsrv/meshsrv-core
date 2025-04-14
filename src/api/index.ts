3867ade7185c5e1dff6109df52cbce3c60fa4fe9
hoywu <wu_git@outlook.com>
2025-04-14 16:55:20 +0800

    feat(api): add CURRENT_SERVER variable to hold the running API server instance

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,12 @@
+import type { Nullable } from '@/util/type';
 import swagger from '@elysiajs/swagger';
 import { file } from 'bun';
 import { Elysia } from 'elysia';
+import type { Server } from 'elysia/universal';
 import { authRoute } from './route/auth';
 import { userRoute } from './route/user';
+
+export let CURRENT_SERVER: Nullable<Server> = null;
 
 export function runApiServer(cert: string, key: string) {
   const apiServer = new Elysia({
@@ -21,5 +25,6 @@ export function runApiServer(cert: string, key: string) {
     .use(authRoute)
     .use(userRoute);
 
+  CURRENT_SERVER = apiServer.server;
   return apiServer.listen(Bun.env.API_SERVER_PORT || 3090);
 }
