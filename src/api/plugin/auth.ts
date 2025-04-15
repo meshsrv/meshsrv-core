571da405624db3a508522f2b2d057674b45143af
hoywu <wu_git@outlook.com>
2025-04-15 20:43:55 +0800

    feat(auth): enhance token retrieval for session validation

--- src/api/plugin/auth.ts
+++ src/api/plugin/auth.ts
@@ -4,7 +4,12 @@ import { Elysia } from 'elysia';
 export const auth = new Elysia({ name: 'Plugin.Auth' }).macro({
   isAuth: {
     async resolve({ headers, error }) {
-      const { session, user } = await validateSessionToken(headers['authorization']);
+      let token = headers['authorization'];
+      if (!token) {
+        // since the browser websocket api does not allow custom headers
+        token = headers['sec-websocket-protocol'];
+      }
+      const { session, user } = await validateSessionToken(token);
       if (!session || !user) return error(401);
       return { session, user };
     },
