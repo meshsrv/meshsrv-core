1f763a1d9231787acbf0bfdc47e4002f022f9cb3
hoywu <wu_git@outlook.com>
2025-04-17 09:51:30 +0800

    feat(auth): add isAgent macro to validate agent tokens

--- src/api/plugin/auth.ts
+++ src/api/plugin/auth.ts
@@ -1,4 +1,5 @@
 import { validateSessionToken } from '@/service/session';
+import { getAgentSecret } from '@/util/agent';
 import { Elysia } from 'elysia';
 
 export const auth = new Elysia({ name: 'Plugin.Auth' }).macro({
@@ -12,6 +13,12 @@ export const auth = new Elysia({ name: 'Plugin.Auth' }).macro({
       const { session, user } = await validateSessionToken(token);
       if (!session || !user) return error(401);
       return { session, user };
+    },
+  },
+  isAgent: {
+    async beforeHandle({ headers, error }) {
+      const token = headers['authorization'] || headers['sec-websocket-protocol'];
+      if (token !== (await getAgentSecret())) return error(401);
     },
   },
 });
