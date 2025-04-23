b6f0eea7d87b6fd3ad18f378173df674fae0d024
hoywu <wu_git@outlook.com>
2025-04-23 17:53:44 +0800

    feat(notification): implement notification list and read endpoints with database integration

--- src/api/route/notification.ts
+++ src/api/route/notification.ts
@@ -1,6 +1,13 @@
-import { Elysia } from 'elysia';
+import { db } from '@/db';
+import { notificationTable } from '@/db/schema';
+import { respt } from '@/util/type';
+import { inArray } from 'drizzle-orm';
+import { createSelectSchema } from 'drizzle-typebox';
+import { Elysia, t } from 'elysia';
 import { auth } from '../plugin/auth';
 import { response } from '../plugin/response';
+
+const notificationSelectSchema = createSelectSchema(notificationTable);
 
 export const notificationRoute = new Elysia()
   .use(auth)
@@ -17,4 +24,39 @@ export const notificationRoute = new Elysia()
         ws.unsubscribe('notification');
       }
     },
-  });
+  })
+  .get(
+    '/notification/list',
+    async ({ suc }) => {
+      return suc(
+        await db.query.notificationTable.findMany({ orderBy: (v, { desc }) => [desc(v.createdAt)] })
+      );
+    },
+    {
+      isAuth: true,
+      response: respt(t.Array(notificationSelectSchema)),
+      detail: {
+        tags: ['notification'],
+      },
+    }
+  )
+  .put(
+    '/notification/read',
+    async ({ body, suc }) => {
+      const result = await db
+        .update(notificationTable)
+        .set({ read: true })
+        .where(inArray(notificationTable.id, body))
+        .then(() => true)
+        .catch(() => false); // TODO: log error
+      return suc(result);
+    },
+    {
+      isAuth: true,
+      body: t.Array(t.Number()),
+      response: respt(t.Boolean()),
+      detail: {
+        tags: ['notification'],
+      },
+    }
+  );
